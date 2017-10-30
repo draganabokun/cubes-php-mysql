b299efd788ba2060316bcd91fcba46f6089ae5ab
Dragana Bokun <lazarevici2013@gmail.com>
2017-10-30 16:14:45 +0100

    Cas 24 - (Baze, pravljenje tabele categories i brands

--- sql/03-SQL-Insert.sql
+++ sql/03-SQL-Insert.sql
@@ -1,3 +1,14 @@
 -- Zadatak: Napisati upit za unos jednog proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, tags, created_at)
+VALUES
+('Apple', 'iPhone6 S', 'iPhone6 S', '', 39999,99, '12.00', 'Mobilni Telefon', 0, 0, '2016-12-22 14:23:05');
 
 -- Zadatak: Napisati upit za unpos vise proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, tags, created_at)
+VALUES
+('Apple', 'iPhone6 S', 'iPhone6 S', '', 39999,99, '12.00', 'Mobilni Telefon', 0, 0, '2016-12-22 14:23:05'),
+
+('Apple', 'iPhone5', 'iPhone5', '', 29999,99, '12.00', 'Mobilni Telefon', 0, 0, '2016-12-25 14:23:05');
+
