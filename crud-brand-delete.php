538faa765afc19e66591ecb8935ce413cf8bfbbe
Dragana Bokun <lazarevici2013@gmail.com>
2017-11-08 15:03:23 +0100

    Cas 28 - Pauza

--- crud-brand-delete.php
+++ crud-brand-delete.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+
+require_once __DIR__ .'/models/m_brands.php';
 
 if (empty($_GET['id']))
 {
@@ -7,30 +10,23 @@ if (empty($_GET['id']))
 
 $id = (int) $_GET['id'];
 
-$link = mysqli_connect('127.0.0.1', 'cubes', 'cubes', 'cubesphp');
-
-if ($link === false) {
-	die('MySQL Error: ' . mysqli_connect_error());
-}
-
-$query = "SELECT * FROM brands WHERE id = '" . mysqli_real_escape_string($link, $id) . "'";
 
 
-$result = mysqli_query($link, $query);
-
-if ($result === false) {
-	die('MySQL Error: ' . mysqli_error($link));
-}
-
-$brand = mysqli_fetch_assoc($result);
+$brand = brandsFetchOneById($id);
 
 if (empty($brand))
 {
     die ('Trazeni brand ne postoji');
 }
 
-print_r($brand);
-die;
+
+if (isset($_POST["task"]) && $_POST["task"] == "delete") {
+   
+    brandsDeleteOneById($id);
+    
+    header('Location: /crud-brand-list.php');
+    die();
+}
 
 require_once __DIR__ .'/views/layout/header.php';
 
--- crud-brand-edit.php
+++ crud-brand-edit.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+
+require_once __DIR__ .'/models/m_brands.php';
+
 
 if (empty($_GET['id']))
 {
@@ -7,22 +11,9 @@ if (empty($_GET['id']))
 
 $id = (int) $_GET['id'];
 
-$link = mysqli_connect('127.0.0.1', 'cubes', 'cubes', 'cubesphp');
 
-if ($link === false) {
-	die('MySQL Error: ' . mysqli_connect_error());
-}
+$brand = brandsFetchOneById($id);
 
-$query = "SELECT * FROM brands WHERE id = '" . mysqli_real_escape_string($link, $id) . "'";
-
-
-$result = mysqli_query($link, $query);
-
-if ($result === false) {
-	die('MySQL Error: ' . mysqli_error($link));
-}
-
-$brand = mysqli_fetch_assoc($result);
 
 if (empty($brand))
 {
@@ -70,17 +61,10 @@ if (isset($_POST["task"]) && $_POST["task"] == "save") {
 	//Ukoliko nema gresaka 
 	if (empty($formErrors)) {
 		//Uradi akciju koju je korisnik trazio
-            $query = "UPDATE brands SET ";
-            $query .= "title = '" . mysqli_real_escape_string($link, $formData['title']) . "',";
-            $query .= "website_url = '" . mysqli_real_escape_string($link, $formData['website_url']) . "' ";
-            $query .= "WHERE id = '" . mysqli_real_escape_string($link, $brand['id']) . "'";
 
+                brandsUpdateOneById($brand['id'], $formData);
             
-            $result = mysqli_query($link, $query);
-            if ($result === false)
-            {
-                die('MySQL error: ' . mysqli_error($link));
-            }
+            
             header('Location: /crud-brand-list.php');
             die;
 	}
